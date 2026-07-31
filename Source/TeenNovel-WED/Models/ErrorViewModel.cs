01d3529b36d4360162cc9c264e2eda42299a3d32
dangle09 <danglntb02258@gmail.com>
2026-07-31 14:53:31 +0700

    xóa code

--- Source/TeenNovel-WED/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TeenNovel_WED.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Source/TeenNovel_Wed/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TeenNovel_Wed.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
